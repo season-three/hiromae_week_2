cc10caf1bdc38182bdbb3edf12826958bc7ab94f
HirokiMaekawa1 <mkhk.31.ne@gmail.com>
2020-07-02 19:17:47 +0900

    update

---
+++ hiromae_week_5/go.mod
@@ -0,0 +1,5 @@
+module github.com/season-three/hiromae_week_5
+
+go 1.14
+
+require github.com/sclevine/agouti v3.0.0+incompatible
---
+++ hiromae_week_5/secret/secret.go
@@ -0,0 +1,11 @@
+package secret
+
+//Myusername 関数の初めは大文字にする
+func Myusername() string {
+	return "mkhk.31.ne@gmail.com"
+}
+
+//Mypassword 関数の初めは大文字にする
+func Mypassword() string {
+	return "11194222"
+}
