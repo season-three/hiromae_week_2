8f40d5937b69501f8c02cc3534254c3dbc99767f
HirokiMaekawa1 <mkhk.31.ne@gmail.com>
2020-07-02 21:23:43 +0900

    update

--- hiromae_week_5/secret/secret.go
+++ hiromae_week_5/secret/secret.go
@@ -1,11 +1,5 @@
 package secret
 
-//Myusername 関数の初めは大文字にする
-func Myusername() string {
-	return "mkhk.31.ne@gmail.com"
-}
-
-//Mypassword 関数の初めは大文字にする
-func Mypassword() string {
-	return "11194222"
+func MyPassword() string {
+	return "XXX"
 }
